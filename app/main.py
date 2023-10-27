0f38b0bf54ad3e874ec5d03b4f848f2cdbf05957
Pärviz <parviz.alizade@gmail.com>
2023-10-27 22:29:37 +0200

    app working

--- app/main.py
+++ app/main.py
@@ -1,62 +1,43 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-from app.model.scoring_script import run
+import pandas as pd
+import pickle
+import numpy as np
+
 
 app = FastAPI()
 
-class Item(BaseModel):
-    uuid: str
-    default: float
-    account_amount_added_12_24m: int
-    account_days_in_dc_12_24m: float
-    account_days_in_rem_12_24m: float
-    account_days_in_term_12_24m: float
-    account_incoming_debt_vs_paid_0_24m: float
-    account_status: float
-    account_worst_status_0_3m: float
-    account_worst_status_12_24m: float
-    account_worst_status_3_6m: float
-    account_worst_status_6_12m: float
-    age: int
-    avg_payment_span_0_12m: float
-    avg_payment_span_0_3m: float
-    merchant_category: str
-    merchant_group: str
-    has_paid: bool
-    max_paid_inv_0_12m: float
-    max_paid_inv_0_24m: float
-    name_in_email: str
-    num_active_div_by_paid_inv_0_12m: float
-    num_active_inv: int
-    num_arch_dc_0_12m: int
-    num_arch_dc_12_24m: int
-    num_arch_ok_0_12m: int
-    num_arch_ok_12_24m: int
-    num_arch_rem_0_12m: int
-    num_arch_written_off_0_12m: float
-    num_arch_written_off_12_24m: float
-    num_unpaid_bills: int
-    status_last_archived_0_24m: int
-    status_2nd_last_archived_0_24m: int
-    status_3rd_last_archived_0_24m: int
-    status_max_archived_0_6_months: int
-    status_max_archived_0_12_months: int
-    status_max_archived_0_24_months: int
-    recovery_debt: int
-    sum_capital_paid_account_0_12m: int
-    sum_capital_paid_account_12_24m: int
-    sum_paid_inv_0_12m: int
-    time_hours: float
-    worst_status_active_inv: float
+column_names = ['uuid', 'default', 'account_amount_added_12_24m',
+       'account_days_in_dc_12_24m', 'account_days_in_rem_12_24m',
+       'account_days_in_term_12_24m', 'account_incoming_debt_vs_paid_0_24m',
+       'account_status', 'account_worst_status_0_3m',
+       'account_worst_status_12_24m', 'account_worst_status_3_6m',
+       'account_worst_status_6_12m', 'age', 'avg_payment_span_0_12m',
+       'avg_payment_span_0_3m', 'merchant_category', 'merchant_group',
+       'has_paid', 'max_paid_inv_0_12m', 'max_paid_inv_0_24m', 'name_in_email',
+       'num_active_div_by_paid_inv_0_12m', 'num_active_inv',
+       'num_arch_dc_0_12m', 'num_arch_dc_12_24m', 'num_arch_ok_0_12m',
+       'num_arch_ok_12_24m', 'num_arch_rem_0_12m',
+       'num_arch_written_off_0_12m', 'num_arch_written_off_12_24m',
+       'num_unpaid_bills', 'status_last_archived_0_24m',
+       'status_2nd_last_archived_0_24m', 'status_3rd_last_archived_0_24m',
+       'status_max_archived_0_6_months', 'status_max_archived_0_12_months',
+       'status_max_archived_0_24_months', 'recovery_debt',
+       'sum_capital_paid_account_0_12m', 'sum_capital_paid_account_12_24m',
+       'sum_paid_inv_0_12m', 'time_hours', 'worst_status_active_inv']
 
-class PredictionOut(BaseModel):
-    score: float
+
+# Load the trained model
+with open("app/model/lg_pipeline.pkl", "rb") as model_file:
+    model = pickle.load(model_file)
 
 @app.get("/")
-def home():
-    return {"health_check": "OK"}
+async def root():
+    return {"Health check": "OK"}
 
-@app.post("/predict", response_model=PredictionOut)
-def predict(payload: Item):
-    score = run(payload)
-    return {"Score: ": score}
+@app.post("/predict/")
+async def predict_prob_default(input_data: dict):
+    # Assuming input_data is a dictionary with the required input columns
+    # Example: {"feature1": 0.5, "feature2": 0.3}
+    inputs = pd.DataFrame(input_data,columns=column_names,index=[0])
+    prob_default = model.predict_proba(inputs)
+    return {"probability_of_default": prob_default}
